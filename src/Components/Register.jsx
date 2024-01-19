7d44161bca9bbfe128d5b798d855cab959beff60
Adi2895 <jangraaditya11@gmail.com>
2024-01-19 22:29:28 +0530

    Register updated

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -10,6 +10,56 @@ export default function Register() {
     setCredentials({ ...credentials, [e.target.name]: e.target.value });
   };
 
+  function isValidEmail(email) {
+    const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    // Test the email address against the regular expression pattern.
+    return regex.test(email); // const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  }
+
+  const submitForm = async(e)=>{
+    e.preventDefault();
+
+    if(credentials.email === ""){
+      alert("Please enter the email address");
+    } else if(credentials.password === ""){
+      alert("Please enter the password")
+    } else {
+        if(!isValidEmail(credentials.email)) {
+          alert("Invalid email");
+          return;
+        } else {
+          alert("reached succes");
+          
+          const response = await fetch(`${port}/authenticate/register`, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              email: credentials.email,
+              password: credentials.password,
+            }),
+          });
+    
+          const json = await response.json();
+          
+          if (response.status === 500 || response.status === 400) {
+            if (response.status === 500) {
+              alert("Innternal server error");
+            } else {
+              alert(json.msg);
+            }
+          } else {
+           
+            alert('Great Account Created Successfully');
+            navigate("/login");
+          }
+        }
+    }
+  }
+
+
   return (
     <div  className=" my-5 container d-flex align-items-center justify-content-center ">
       <div className="d-flex flex-column justify-content-center align-items-center glass">
@@ -18,7 +68,7 @@ export default function Register() {
           Explore More by connecting with us.
         </small>
 
-        <form className="py-1 ">
+        <form className="py-1 " onSubmit={(e)=>(submitForm(e))}>
           <div className="textbox flex flex-column g text-center ">
             <div style={{marginLeft:"10px"}}>
             <input
@@ -28,6 +78,7 @@ export default function Register() {
               value={credentials.email}
               className="form-control form-control-lg inputpassword"
               onChange={onChange}
+              style={{fontSize:"2.9px"}}
               placeholder="Enter a valid email address"
               required
             />
@@ -37,6 +88,7 @@ export default function Register() {
               onChange={onChange}
               className=" mb-3 mt-4 form-control form-control-lg inputpassword"
               type="password"
+              style={{fontSize:"2.9px"}}
               placeholder="Password"
               name="password"
             />
@@ -45,6 +97,8 @@ export default function Register() {
               className="btn btn-lg signup-button"
               onClick={(e) => {
                 e.preventDefault();
+                alert("hii")
+                submitForm(e)
                 // Handle signup logic here
               }}
             >
